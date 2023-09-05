5d7b2ebb0bce2cfae6679ee8d655ac219e2c12b0
rotteralec <rotteralec@gmail.com>
2023-09-05 12:04:37 -0600

    Added Three scripts and updated ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # MerakiLogDataTransformation
- Simple python scritps to ingest meraki .log files and format it into usable data by PowerBI
+ Simple python scripts to ingest meraki .log files and format it into usable data by PowerBI
+ Three Total Scripts:
+ 1 log_sorter_retro.py:
+    Retroactively goes through all .log files in the folder it is hard coded to point to.
+
+ 2 log_sorter_test.py:
+    Test version of daily script.
+    Do not use output for actual data analytics
+
+ 3 log_sorter.py:
+    Production version of daily script.
+    **Use this output for data analytics
---
+++ log_sorter_retro.py
@@ -0,0 +1,193 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 18 10:10:57 2023
+
+@author: arotter
+"""
+
+
+##To do now:
+###*Fix formatting
+##Run on all files in folder
+## 
+
+##To do long:
+##*Add to headers from parsing
+##*user based****
+##Optimize script performance
+#
+#
+
+import pandas as pd
+##Use uuid.uuid4() for random
+##in future use uuid5 with sha-1 hash of uuid and name/data/time??
+import uuid
+import os
+
+    
+    
+eventarr = []
+xarr = []
+firewallarr = []
+vpnfirewallarr = []
+flowsarr = []
+ipflowarr = []
+urlarr = []
+otherarr = []
+#fieldNames = ("type", 'radio', 'vap')
+counter = 0
+headers = ["uuid", "date", "time", "IP", "6", "7", "8", "location", "log_class", "type", "radio", "vap", "client_mac", "client_ip", "client_ip6", "identity", "aid"]
+
+
+xsucarr = []
+xautharr = []
+xdeautharr = []
+xclientdeautharr = []
+folder_path = "C:/Users/arotter/Documents/winsyslog"
+
+xsucarr.append(headers)
+def get_dir():
+    os.chdir(folder_path)
+    for file in os.listdir():
+        if file.endswith(".log"):
+            file_path = f"{folder_path}/{file}"
+            print(file_path)
+            read_log(file_path)
+
+def read_log(_logs):
+    with open(_logs) as f:
+        data = f.read().splitlines()
+        txt_parse(data)
+        
+
+def val_add(cat):
+    return headers.index(cat)
+
+
+
+
+def field_seperator(temp_arr, _data):
+    
+    temp_arr[0:9] = _data[0:9]
+    for i in _data[9:]:
+        temp_cat = ""
+        temp_val = ""
+        equal_ind = -1
+        equal_ind = i.find('=')
+        
+        if(equal_ind != -1):
+            temp_cat = str(i[0:equal_ind])
+            temp_val = str(i[equal_ind+1:])
+            temp_arr[val_add(temp_cat)] = temp_val
+    
+
+
+
+def event_sorter( event_data):
+    each_log = [""]*20
+    
+    if(event_data[9] == "type=8021x_eap_success"):
+        field_seperator(each_log, event_data)
+        #del each_log[0:1]
+        xsucarr.append(each_log)
+    elif(event_data[10] == "type=8021x_auth"):
+        xautharr.append(event_data)
+    elif(event_data[10] == "type=8021x_client_deauth"):
+        xclientdeautharr.append(event_data)
+    elif(event_data[10] == "type=8021x_deauth"):
+        xdeautharr.append(event_data)
+    else:
+        eventarr.append(event_data)
+
+
+    
+
+
+
+
+def txt_parse(_data):
+    #print(_data)
+    for i in _data:
+        temp = []
+        parts = i.split(',')
+        temp.append(str(uuid.uuid4()))
+        for k in parts[2:7]:
+            temp.append(k)
+        
+        parts[7] = parts[7].split(' ')
+        for k in parts[7]:
+            temp.append(k)
+        if(temp[8] == "events"):
+            event_sorter(temp)
+
+
+get_dir()
+print(xsucarr)
+pd.DataFrame(xsucarr).to_csv('xsuccess_all.csv')
+
+#event_sorter(data)
+#print(eventarr)
+""" print(xautharr)
+print(xdeautharr)
+print(xclientdeautharr)
+print(xsucarr) """
+""" for i in data:
+    temp = []
+    parts = i.split(',')
+    if(parts[10] == "urls"):
+        urlarr.append(parts)
+
+    elif(parts[10] == "firewall"):
+        firewallarr.append(parts)
+    elif(parts[10] == "flows"):
+        flowsarr.append(parts)
+
+    elif(parts[10] == "vpn_firewall"):
+        vpnfirewallarr.append(parts)
+    elif(parts[10] == "events"):
+        eventarr.append(parts)
+    elif(parts[10] == "ip_flow_start"):
+        ipflowarr.append(parts)
+    elif(parts[10] == "ip_flow_end"):
+        ipflowarr.append(parts)
+    else:
+        otherarr.append(parts)
+     """
+
+
+
+
+
+
+
+
+
+
+
+
+
+#print(urlarr)
+#pd.DataFrame(urlarr).to_csv('urls.csv')
+#print(firewallarr)
+#pd.DataFrame(firewallarr).to_csv('firewall.csv')
+#print(flowsarr)
+
+
+
+
+
+
+
+#print(vpnfirewallarr)
+#pd.DataFrame(vpnfirewallarr).to_csv('vpnfirewall.csv')
+#print(eventarr)
+#pd.DataFrame(eventarr).to_csv('events_test.csv')
+#pd.DataFrame(xarr).to_csv('x_test.csv')
+#print(ipflowarr)
+#pd.DataFrame(ipflowarr).to_csv('ipflows.csv')
+#print(otherarr)
+#pd.DataFrame(otherarr).to_csv('OTHERLOGS.csv')
+#print(otherarr)
+#print(counter)
+
+    
